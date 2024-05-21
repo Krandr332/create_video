fc5f6020e38d31fc68724fde0e0152178806c920
Андрюша <57506848+Krandr332@users.noreply.github.com>
2024-05-21 14:29:44 +0300

    1

--- test_task/video_app/forms.py
+++ test_task/video_app/forms.py
@@ -1,4 +1,4 @@
 from django import forms
 
 class VideoForm(forms.Form):
-    text = forms.CharField(label='Text for Marquee', max_length=255)
+    text = forms.CharField(label='text for the video', max_length=255)
